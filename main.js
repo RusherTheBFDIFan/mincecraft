7099764fda3f23f3b31f2b56e383c2868a742c31
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2021-06-09 20:41:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var canvas = new fabric.Canvas("myCanvas");
 
 block_width = 30;
@@ -34,4 +35,90 @@ function new_image(get_image){
         });
         canvas.add(blockimage_object);
     });
+}
+
+window.addEventListener("keydown",mykeydown);
+
+function mykeydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (e.shiftKey == true && keyPressed == '80'){
+        console.log("The p key and the shift key are pressed together");
+        block_height = block_height + 10;
+        block_width = block_width + 10;
+        document.getElementById("currentwidth").innerHTML=block_width;
+        document.getElementById("currentheight").innerHTML=block_height;
+    }
+    if (e.shiftKey == true && keyPressed == '77'){
+        console.log("The m key and the shift key are pressed together");
+        block_height = block_height - 10;
+        block_width = block_width - 10;
+        document.getElementById("currentwidth").innerHTML=block_width;
+        document.getElementById("currentheight").innerHTML=block_height;
+    }
+
+    if (keyPressed == '38'){
+        up();
+        console.log("up");
+    }
+
+    if (keyPressed == '40'){
+        down();
+        console.log("down");
+    }
+
+    if (keyPressed == '37'){
+        left();
+        console.log("left");
+    }
+
+    if (keyPressed == '39'){
+        right();
+        console.log("right");
+    }
+
+    if (keyPressed == '87'){
+        new_image("wall.jpg");
+        console.log("w");
+    }
+
+    if (keyPressed == '71'){
+        new_image("ground.png");
+        console.log("g");
+    }
+
+    if (keyPressed == '76'){
+        new_image("light_green.png");
+        console.log("l");
+    }
+
+    if (keyPressed == '68'){
+        new_image("dark_green.png");
+        console.log("d");
+    }
+
+    if (keyPressed == '84'){
+        new_image("trunk.jpg");
+        console.log("t");
+    }
+
+    if (keyPressed == '67'){
+        new_image("cloud.jpg");
+        console.log("c");
+    }
+
+    if (keyPressed == '89'){
+        new_image("yellow_wall.png");
+        console.log("y");
+    }
+
+    if (keyPressed == '85'){
+        new_image("unique.png");
+        console.log("u");
+    }
+
+    if (keyPressed == '82'){
+        new_image("roof.jpg");
+        console.log("r");
+    }
 }
